09aca747c6c2ac6373b654ad26f75badcb156f13
Vitali Dzervanouski <vitalydervanowsky@gmail.com>
2021-08-14 20:15:05 +0300

    add Task2 (dark theme) from dz22

--- app/src/main/java/com/example/tmsandroid/MenuFragment.kt
+++ app/src/main/java/com/example/tmsandroid/MenuFragment.kt
@@ -14,6 +14,7 @@ import com.example.tmsandroid.dz.dz18.Dz18Fragment
 import com.example.tmsandroid.dz.dz19.Dz19Fragment
 import com.example.tmsandroid.dz.dz20.Dz20Fragment
 import com.example.tmsandroid.dz.dz21.Dz21Fragment
+import com.example.tmsandroid.dz.dz22.Dz22Fragment
 
 class MenuFragment : Fragment() {
     private var _binding: FragmentMenuBinding? = null
@@ -30,60 +31,69 @@ class MenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.bDz14.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz14Fragment())
-                .commit()
-        }
+        binding.apply {
+            bDz14.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz14Fragment())
+                    .commit()
+            }
 
-        binding.bDz15.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz15Fragment())
-                .commit()
-        }
+            bDz15.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz15Fragment())
+                    .commit()
+            }
 
-        binding.bDz16.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz16Fragment())
-                .commit()
-        }
+            bDz16.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz16Fragment())
+                    .commit()
+            }
 
-        binding.bDz17.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz17Fragment())
-                .commit()
-        }
-        
-        binding.bDz18.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz18Fragment())
-                .commit()
-        }
-        
-        binding.bDz19.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz19Fragment())
-                .commit()
-        }
+            bDz17.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz17Fragment())
+                    .commit()
+            }
 
-        binding.bDz20.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz20Fragment())
-                .commit()
-        }
+            bDz18.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz18Fragment())
+                    .commit()
+            }
 
-        binding.bDz21.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.container, Dz21Fragment())
-                .commit()
+            bDz19.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz19Fragment())
+                    .commit()
+            }
+
+            bDz20.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz20Fragment())
+                    .commit()
+            }
+
+            bDz21.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz21Fragment())
+                    .commit()
+            }
+
+            bDz22.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.container, Dz22Fragment())
+                    .commit()
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/tmsandroid/dz/dz22/Dz22Fragment.kt
@@ -0,0 +1,41 @@
+package com.example.tmsandroid.dz.dz22
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.tmsandroid.databinding.FragmentDz22Binding
+
+class Dz22Fragment : Fragment() {
+    private var _binding: FragmentDz22Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDz22Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            bThemeLight.setOnClickListener {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            bThemeDark.setOnClickListener {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
